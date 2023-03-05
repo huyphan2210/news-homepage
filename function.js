f9e93ffeb93506889c0637b832f5517042ac00a3
Phan Võ Minh Huy <123850680+huyphan2210@users.noreply.github.com>
2023-03-05 08:38:32 +0700

    Handling turn mobile menu on/off events

--- function.js
+++ function.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
     $('header img:nth-child(2)').click(function() {
-        const displayMobileMenu = $('.mobileMenu').css('display');
-        if (displayMobileMenu === 'none') {
-            $('.mobileMenu').css('display', 'block');
-            $('.mobileMenu').css('transition', 'transform 2s');
+        const mobileMenuLeft = $('.mobileMenu').css('left').substring(0, $('.mobileMenu').css('left').length - 2);
+        console.log($(window).width() / parseFloat(mobileMenuLeft))
+        if (Math.round($(window).width() / parseFloat(mobileMenuLeft)) === 1) {
+            $('.overlay').css('display', 'block');
             $('.mobileMenu').css('transform', 'translate(-100%)');
         }
+    })
+
+    $('.overlay').click(function() {
+        $('.mobileMenu').css('transform', 'translate(0)');
+        $('.overlay').css('display', 'none');
+    })
+
+    $('.mobileMenu img').click(function() {
+        $('.mobileMenu').css('transform', 'translate(0)');
+        $('.overlay').css('display', 'none');
     })
 })
